f88d4a7f72d366c673c13067dc6a4688a2334c79
vn271 <tuthancodon@gmail.com>
2017-10-12 11:28:24 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-./miner --server  zec-asia1.nanopool.org --user t1YQMTEvoKVysdCvLaxtnHppfnpk9rJuFJK.Zcash/fk.anmay@gmail.com
- --pec --fee 0 --pass z --port 6666 --api 0.0.0.0
+./miner --server  zec-asia1.nanopool.org --user t1YQMTEvoKVysdCvLaxtnHppfnpk9rJuFJK.Zcash/fk.anmay@gmail.com --pass z --port 6666 --api 0.0.0.0:42000
