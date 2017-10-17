841789c5997ef81713efbe78d5f8b21f536d28db
vn271 <tuthancodon@gmail.com>
2017-10-17 09:01:05 +0700

    Update enable_nvidia_fan_control.sh

--- enable_nvidia_fan_control.sh
+++ enable_nvidia_fan_control.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo nvidia-xconfig --enable-all-gpus
-sudo nvidia-xconfig --cool-bits=28
+#sudo nvidia-xconfig --cool-bits=28
+sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
