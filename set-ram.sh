cdc85c1d99e661996b0494505b1f141567abb6e6
vn271 <tuthancodon@gmail.com>
2017-10-18 15:23:44 +0700

    Update set-ram.sh

--- set-ram.sh
+++ set-ram.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sudo echo "vm.swappiness=10" >> /etc/sysctl.conf
+sudo echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
 sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
