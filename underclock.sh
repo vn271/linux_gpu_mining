936dfecc5d9e3300e011f9cc60ab8b406cb75ed4
vn271 <tuthancodon@gmail.com>
2017-10-18 11:29:34 +0700

    Update underclock.sh

--- underclock.sh
+++ underclock.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
+sudo nvidia-settings -a GpuPowerMizerMode=1
 sudo nvidia-settings -a GPUFanControlState=1
 sudo nvidia-settings -a GPUTargetFanSpeed=80
 sudo nvidia-settings -a GPUPowerMizerMode=1
 sudo nvidia-settings -a GPUGraphicsClockOffset[3]=50
+sudo nvidia-settings -a GPUMemoryTransferRateOffset[3]=1000
 
 sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl 250
-
