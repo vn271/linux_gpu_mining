6499b8d335c94262441c8b9c2dde0fd3fed5b7c8
vn271 <tuthancodon@gmail.com>
2017-10-24 16:20:30 +0700

    Update overclock.js

--- overclock.js
+++ overclock.js
@@ -9,7 +9,7 @@ var cmd = "DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-settings -
 cmd += " -a GPUFanControlState=1";
 cmd += " -a GPUTargetFanSpeed=100";
 cmd += " -a GPUGraphicsClockOffset[3]=86";
-cmd += " -a GPUMemoryTransferRateOffset[3]=608";
+cmd += " -a GPUMemoryTransferRateOffset[3]=368";
 cmd += " -a GPUFanControlState=1";
 cmd += " -a GPUFanControlState=1";
 
