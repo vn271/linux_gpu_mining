1f50d4b5dbee151d9f6ac19ab80ffb313ac43560
vn271 <tuthancodon@gmail.com>
2017-10-25 23:28:25 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 
-/home/cash/linux_gpu_mining/miner --server  zec-asia1.nanopool.org --user t1YQMTEvoKVysdCvLaxtnHppfnpk9rJuFJK.Zcash/fk.anmay@gmail.com --pass z --fee 0 --port 6666 --api 0.0.0.0:42000
+/home/cash/linux_gpu_mining/miner --server  zec-asia1.nanopool.org --user t1YQMTEvoKVysdCvLaxtnHppfnpk9rJuFJK.Zcash/fk.anmay@gmail.com --templimit 85 --pass z --fee 0 --port 6666 --api 0.0.0.0:42000
