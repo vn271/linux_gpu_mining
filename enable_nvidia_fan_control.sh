edb6fb6a2c52573c10ee9a3b107ecaded2d52a90
vn271 <tuthancodon@gmail.com>
2017-10-25 23:21:13 +0700

    Update enable_nvidia_fan_control.sh

--- enable_nvidia_fan_control.sh
+++ enable_nvidia_fan_control.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+nvidia-xconfig -s -a --allow-empty-initial-configuration --cool-bits=28 --registry-dwords="PerfLevelSrc=0x2222"--no-sli --connected-monitor="DFP-0" -o /etc/X11/xorg.conf
+sed -i '/Driver/a Option "Interactive" "False"' /etc/X11/xorg.conf
 DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig --enable-all-gpus
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig --cool-bits=28
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig --cool-bits=28
 #DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
