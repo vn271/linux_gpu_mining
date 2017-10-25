b7fac718fcc8c61fbee4d2adf67f700fcd7db39f
vn271 <tuthancodon@gmail.com>
2017-10-26 00:55:42 +0700

    Update overclock.js

--- overclock.js
+++ overclock.js
@@ -2,5 +2,5 @@
 'use strict';
 var shell = require('shelljs');
 var sleep = require('sleep');
-sleep.sleep(10); // sleep for ten seconds
+sleep.sleep(3); // sleep for ten seconds
 shell.exec('bash /home/cash/linux_gpu_mining/underclock.sh', function(code, stdout, stderr) {  console.log(code); });
