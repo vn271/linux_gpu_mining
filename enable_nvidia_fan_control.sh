c326ee7ca3b317ba7f84704ab938f2ecf82c8008
vn271 <tuthancodon@gmail.com>
2017-10-25 22:27:19 +0700

    Update enable_nvidia_fan_control.sh

--- enable_nvidia_fan_control.sh
+++ enable_nvidia_fan_control.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo nvidia-xconfig --enable-all-gpus
-sudo nvidia-xconfig --cool-bits=28
-#sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
+DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig --enable-all-gpus
+DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig --cool-bits=28
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
