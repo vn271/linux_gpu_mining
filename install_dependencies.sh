62ac5f0072ae7897e0b6c0b6637ec7630d6cd93b
vn271 <tuthancodon@gmail.com>
2017-10-26 13:03:27 +0700

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,12 +4,12 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update 
 sudo apt -y full-upgrade
-sudo apt-get install git google-chrome-stable
+sudo apt-get install google-chrome-stable
 sudo apt-get install nvidia-384 nvidia-libopencl1-384 nvidia-opencl-icd-384
 sudo apt-get install screen
-sudo apt-get install openssh-server
 sudo apt-get install npm
 sudo apt-get install nodejs-legacy
 cd /home/cash/linux_gpu_mining
 npm install --save shelljs
 npm install --save sleep
+sudo apt-get install openssh-server
