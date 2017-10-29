f793f195634cf790111c6a56bc10337c8a70e8f9
Cuong <luk3.pro@me.com>
2017-10-29 14:28:52 +0700

    Add reboot file

---
+++ reboot.sh
@@ -0,0 +1,3 @@
+/usr/bin/screen -dmS "miner" /home/cash/linux_gpu_mining/start.sh
+/usr/bin/screen -dmS "client" node /home/cash/linux_gpu_mining/client.js
+/home/cash/linux_gpu_mining/underclock.sh
