a220faf2b21490794783fbc136f3675fa42dba8c
Cuong <luk3.pro@me.com>
2017-10-29 14:19:19 +0700

    Add client

--- overclock.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-var shell = require('shelljs');
-var sleep = require('sleep');
-sleep.sleep(3); // sleep for ten seconds
-shell.exec('bash /home/cash/linux_gpu_mining/underclock.sh', function(code, stdout, stderr) {  console.log(code); });
