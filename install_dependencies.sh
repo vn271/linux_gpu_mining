8674cfb4402cf90a483cb1c015a67418357334d1
vn271 <tuthancodon@gmail.com>
2018-01-03 08:21:27 +0700

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,8 +9,8 @@ sudo apt-get install -y curl screen libcurl4-openssl-dev libssl-dev libjansson-d
 curl -sL https://deb.nodesource.com/setup_9.x | sudo bash -
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-sudo apt-get update
-sudo apt -y full-upgrade
+sudo apt-get update && apt-get upgrade -y && apt-get -y full-upgrade && apt-get autoremove && apt-get autoclean
+
 sudo apt install -y nvidia-cuda-toolkit
 sudo apt-get install -y cuda-8.0
 sudo ln -s /usr/lib/libOpenCL.so.1 /usr/lib/libOpenCL.so
