06cd20b1b9a4e09a05aed271b5b435cc3ccb59d0
vn271 <tuthancodon@gmail.com>
2018-01-15 22:00:23 +0700

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,11 +8,11 @@ sudo apt-get update
 sudo apt-get install -y curl screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 curl -sL https://deb.nodesource.com/setup_9.x | sudo bash -
 sudo apt-get update && apt-get upgrade -y && apt-get -y full-upgrade && apt-get autoremove && apt-get autoclean
-sudo apt-get install -y nvidia-384 nvidia-libopencl1-384 nvidia-opencl-icd-384
-sudo ln -s /usr/lib/libOpenCL.so.1 /usr/lib/libOpenCL.so
+#sudo apt-get install -y nvidia-384 nvidia-libopencl1-384 nvidia-opencl-icd-384
+#sudo ln -s /usr/lib/libOpenCL.so.1 /usr/lib/libOpenCL.so
 
 sudo apt-get install -y nodejs 
-nodejs-legacy npm
-npm install forever -g
-npm install gulp -g
+apt install -y nodejs-legacy npm
+npm install -g forever
+npm install -g gulp
 npm install -g shelljs
